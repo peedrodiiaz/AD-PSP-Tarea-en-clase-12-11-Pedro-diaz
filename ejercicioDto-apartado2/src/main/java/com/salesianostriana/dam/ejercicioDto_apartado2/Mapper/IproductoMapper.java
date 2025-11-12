1637c81b6cbe5189c7755cfbeb834c2b4b0e2446
Pedro <diaz.gaped24@triana.salesianos.edu>
2025-11-12 13:12:46 +0100

    arreglo

--- ejercicioDto-apartado2/src/main/java/com/salesianostriana/dam/ejercicioDto_apartado2/Mapper/IproductoMapper.java
+++ ejercicioDto-apartado2/src/main/java/com/salesianostriana/dam/ejercicioDto_apartado2/Mapper/IproductoMapper.java
@@ -1,1 +1,11 @@
-package com.salesianostriana.dam.ejercicioDto_apartado2.Mapper;import com.salesianostriana.dam.ejercicioDto_apartado2.Dto.ProductoDto;import com.salesianostriana.dam.ejercicioDto_apartado2.Models.Producto;import org.mapstruct.Mapper;@Mapper(componentModel = "spring")public interface IproductoMapper {    ProductoDto todto (Producto producto);    Producto toEntity (ProductoDto productoDto);}
+package com.salesianostriana.dam.ejercicioDto_apartado2.Mapper;
+
+import com.salesianostriana.dam.ejercicioDto_apartado2.Dto.ProductoDto;
+import com.salesianostriana.dam.ejercicioDto_apartado2.Models.Producto;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface IproductoMapper {
+    ProductoDto todto (Producto producto);
+    Producto toEntity (ProductoDto productoDto);
+}
