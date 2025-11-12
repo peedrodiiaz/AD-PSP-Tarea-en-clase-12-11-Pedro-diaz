5733efad82dadfbfe7857fe822d2a0e2cc1828ec
Pedro <diaz.gaped24@triana.salesianos.edu>
2025-11-12 13:09:25 +0100

    arreglo

--- ejercicioDto-apartado1/src/main/java/com/salesianostriana/dam/ejercicioDto_apartado1/Dto/AlumnoDto.java
+++ ejercicioDto-apartado1/src/main/java/com/salesianostriana/dam/ejercicioDto_apartado1/Dto/AlumnoDto.java
@@ -1,1 +1,8 @@
-package com.salesianostriana.dam.ejercicioDto_apartado1.Dto;import com.salesianostriana.dam.ejercicioDto_apartado1.Model.Curso;import com.salesianostriana.dam.ejercicioDto_apartado1.Model.Direccion;public record AlumnoDto( String nombre, String apellido1, String apellido2,                        int telefono, String email, Direccion direccion, Curso curso                         ) {}
+package com.salesianostriana.dam.ejercicioDto_apartado1.Dto;
+
+import com.salesianostriana.dam.ejercicioDto_apartado1.Model.Curso;
+import com.salesianostriana.dam.ejercicioDto_apartado1.Model.Direccion;
+
+public record AlumnoDto( String nombre, String apellido1, String apellido2,
+                        int telefono, String email, Direccion direccion, Curso curso
+                         ) {}
